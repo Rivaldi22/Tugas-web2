ef18ad57633ae76d851416e856831b7eaa92aa73
Rivaldi Hidayatullah <113918511+Rivaldi22@users.noreply.github.com>
2024-05-22 23:24:57 +0700

    Add files via upload

---
+++ pertemuan1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Biodata PHP</title>
+</head>
+
+<body>
+    <?php
+    print "<pre>";
+    print "<center><font size=14 color='magenta'>BIODATA</font></center>";
+    echo "<hr size=4 width=80% color='blue'>";
+    print "<center right><font size=12 color='red'> Nama : Rivaldi Hidayatullah </center right>
+</font><br>";
+    print "<center><font size=12 color='blue'> Kelas : 06TPLM005 </center>
+</font><br>";
+    print "<center right><font size=12 color='green'> Jurusan : Teknik Informatika </center right>
+    </font><br>";
+    print "<center right><font size=12 color='green'> Instansi : Universitas Pamulang </center right>
+    </font><br>";
+    print "<center right><font size=12 color='purple'> Hoby : Hiking
+    </center right></font><br></pre>";
+    echo "<hr size=4 width=80% color='blue'>";
+    ?>
+
+    <?php
+    print "<center><font face='arial' size=5 color='black'>Kesan: Sangat menyenangkan karena dapat ilmu baru sehingga dapat mengembangkan skill
+    yang sudah ada sebelumnya.</font></center>";
+    ?>
+</body>
+
+</html>
